b8b2d8888478677f11d1e1e2027ccb10f5ecf110
Aditya Vikram KIrtania <108626210+Aditya792max@users.noreply.github.com>
2025-10-25 21:36:51 +0530

    third3Commit

--- src/SingleNumberThree.java
+++ src/SingleNumberThree.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class SingleNumberThree {
+import java.util.*;
+public class SingleNumberThree {
+    public static int [] snt(int [] nums){
+        Set<Integer> set = new HashSet<>();
+        for(int num:nums){
+            if(set.contains(num)){
+                set.remove(num);
+            }else {
+                set.add(num);
+            }
+        }
+        int[] res = set.stream().mapToInt(Integer::intValue).toArray();
+        return res;
+    }
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+        int [] nums = {1,2,1,3,2,5};
+        int [] res = snt(nums);
+        System.out.println(Arrays.toString(res));
+    }
 }
